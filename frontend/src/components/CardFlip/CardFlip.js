b3108edf9bddf313d1f0b896db2b816241ca1cd1
marcus-koch <marcus.koch01@outlook.de>
2021-03-31 22:10:56 +0200

    cleanup disabled code

--- frontend/src/components/CardFlip/CardFlip.js
+++ frontend/src/components/CardFlip/CardFlip.js
@@ -6,20 +6,10 @@ const CardFlip = ({ front, back }) => {
     <div className='flip__container'>
       <div className='flip__container-inner'>
         <figure className='flip__figure'>
-          {/* <img
-            className='flip__img flip__img--front'
-            src={front}
-            alt='CardFlip'
-          /> */}
           <div className='flip__img flip__img--front'>{front}</div>
         </figure>
         <figcaption className='flip__figcaption'>
           <div className='flip__img flip__img--back'>{back}</div>
-          {/* <img
-            className='flip__img flip__img--back'
-            src={back}
-            alt='CardFlip'
-          /> */}
         </figcaption>
       </div>
     </div>
